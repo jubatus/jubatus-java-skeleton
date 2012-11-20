a1e7a1c4bd47d473b8969f93dcda174a2195cf35
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2012-11-20 17:16:57 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+Skelton of Jubatus Client Application in Java
+=============================================
+
+Requirements
+------------
+
+* [Eclipse IDE for Java Developers](http://www.eclipse.org/downloads/)
+    * Tested with Indigo SR2 (3.7.2) and Juno SR1 (4.2.1).
+    * Note: `Java EE Developers` edition does not support Git by default.
+
+Usage
+-----
+
+To import a skelton project to Eclipse IDE:
+
+1. Choose `Import...` from `File` menu.
+2. Select `Git` > `Projects from Git`, then click `Next`.
+3. Select `URI`, then click `Next`.
+4. Fill `URI` field with `https://github.com/jubatus/jubatus-java-skelton.git`, then click `Next`.
+5. Follow the instruction on the dialog and complete the wizard.
+
+Once the import has been completed, Maven will automatically download Jubatus client library and other dependencies.
+
+Now, direct to `src/main/java/` directory.
+You will see `Client.java` in the `(default package)` tree.
+`Client.java` is a simple example that illustrates the usage of Jubatus recommender.
+
+To run the client program, right-click on the `Client.java` and select `Run As` > `Java Application`.
+Make sure you start the `jubarecommender` process before running the client.
---
+++ src/main/java/Client.java
@@ -0,0 +1,113 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import us.jubat.recommender.ConfigData;
+import us.jubat.recommender.Datum;
+import us.jubat.recommender.RecommenderClient;
+import us.jubat.recommender.TupleStringDouble;
+import us.jubat.recommender.TupleStringFloat;
+import us.jubat.recommender.TupleStringString;
+
+public class Client {
+	public static final String HOST = "localhost";
+	public static final int PORT = 9199;
+	public static final String NAME = "";
+	public static final String ALGORITHM = "lsh";
+
+	public void start() throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("config.json")));
+		StringBuilder sb = new StringBuilder();
+		String line;
+		while ((line = br.readLine()) != null) {
+			sb.append(line);
+			sb.append('\n');
+		}
+		br.close();
+
+		ConfigData config = new ConfigData();
+		config.method = ALGORITHM;
+		config.converter = sb.toString();
+
+		RecommenderClient r = new RecommenderClient(HOST, PORT, 5);
+		r.set_config(NAME, config);
+
+		Datum d = new Datum();
+		d.string_values = new ArrayList<TupleStringString>();
+		d.num_values = new ArrayList<TupleStringDouble>();
+
+		TupleStringDouble tuple1, tuple2, tuple3;
+
+		// user01
+		tuple1 = new TupleStringDouble();
+		tuple1.first = "movie_A";
+		tuple1.second = 5;
+		d.num_values.add(tuple1);
+
+		tuple2 = new TupleStringDouble();
+		tuple2.first = "movie_B";
+		tuple2.second = 2;
+		d.num_values.add(tuple2);
+
+		tuple3 = new TupleStringDouble();
+		tuple3.first = "movie_C";
+		tuple3.second = 3;
+		d.num_values.add(tuple3);
+
+		r.update_row(NAME, "user01", d);
+
+		// user02
+		tuple1 = new TupleStringDouble();
+		tuple1.first = "movie_A";
+		tuple1.second = 2;
+		d.num_values.add(tuple1);
+
+		tuple2 = new TupleStringDouble();
+		tuple2.first = "movie_B";
+		tuple2.second = 5;
+		d.num_values.add(tuple2);
+
+		tuple3 = new TupleStringDouble();
+		tuple3.first = "movie_C";
+		tuple3.second = 1;
+		d.num_values.add(tuple3);
+
+		r.update_row(NAME, "user02", d);
+
+		// user03
+		tuple1 = new TupleStringDouble();
+		tuple1.first = "movie_A";
+		tuple1.second = 5;
+		d.num_values.add(tuple1);
+
+		tuple2 = new TupleStringDouble();
+		tuple2.first = "movie_B";
+		tuple2.second = 1;
+		d.num_values.add(tuple2);
+
+		tuple3 = new TupleStringDouble();
+		tuple3.first = "movie_C";
+		tuple3.second = 4;
+		d.num_values.add(tuple3);
+
+		r.update_row(NAME, "user03", d);
+
+		List<String> rows = r.get_all_rows(NAME);
+		for (String id : rows) {
+			List<TupleStringFloat> result;
+			result = r.similar_row_from_id(NAME, id, 3);
+			System.out.println(id + " is similar to: ");
+			System.out.print("  ");
+			for (TupleStringFloat tuple : result) {
+				System.out.print(tuple.first + " (" + tuple.second + "), ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		new Client().start();
+		System.exit(0);
+	}
+}
